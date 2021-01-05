f881e79b5f9c67f085d480076115d271ca268f9b
whalloran <66441541+whalloran@users.noreply.github.com>
2021-01-04 20:58:27 -0500

    Add files via upload

---
+++ nav-responsive.js
@@ -0,0 +1,11 @@
+
+/* Responsive Navigation */
+
+function toggleResponsive() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
